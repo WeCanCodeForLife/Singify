c37911ee81e66a56e8ad97bd1f2695b490594190
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-01-05 10:04:29 -0600

    css for login

--- src/Login.js
+++ src/Login.js
@@ -8,7 +8,7 @@ function Login() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       /> 
-      <button>LOGIN TO SPOTIFY</button>
+      <a>LOGIN TO SPOTIFY</a>
         </div>
     )
 }
