51b73750fccd25849a407964f6b02e08c3ab686a
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-01-05 15:31:39 -0600

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import React, { useEffect } from "react";
 import './App.css';
 import Login from "./Login";
 function App() {
+ 
+  useEffect(() => {
+  }, []);
+
   return (
     <div className="app">
       <Login/>
