ef5455a448fc3d62cf94cf76602689de993ba71f
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-01-06 12:49:03 -0600

    have a token

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
 import './App.css';
 import Login from "./Login";
+import { getTokenFromResponse } from "./spotify";
 function App() {
  
   useEffect(() => {
+    const hash = getTokenFromResponse();
+    console.log("i have a token", hash)
   }, []);
 
   return (
