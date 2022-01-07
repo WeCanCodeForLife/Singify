7fcf9035b21cbad47197733ca1e24223b9b3ede1
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-01-07 10:32:28 -0600

    logged in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
 import { getTokenFromResponse } from "./spotify";
+
 function App() {
- 
+  const [ token , setToken] = useState(null);
   useEffect(() => {
     const hash = getTokenFromResponse();
-    console.log("i have a token", hash)
+    
+    window.location.hash = "";
+    let _token = hash.access_token;
+  
+   
+  if (_token) {
+    setToken(_token);
+    }
+    console.log("i have a token", hash) 
   }, []);
 
   return (
     <div className="app">
-      <Login/>
+      { token ? (<h1>I am logged in</h1>):
+      (<Login/>)}
+
+      
     </div>
   );
 }
