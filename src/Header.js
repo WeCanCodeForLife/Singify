b79540f1a33737b4258fab2823d3b1e8779e3135
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-01-14 11:40:05 -0600

    header left search

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 function Header() {
     return (
         <div className="header">
+         <div className="header__left">
+        <SearchIcon />
+        <input
+          placeholder="Search for Artists, Songs, or Podcasts "
+          type="text"
+        />
+      </div>
+      <div className="header__right">
+      <Avatar alt="" src="" />
+        <h4>name</h4>
+      </div>
+    </div>
             
-        </div>
     )
 }
 
